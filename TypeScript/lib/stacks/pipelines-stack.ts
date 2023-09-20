d8d4694bbc056216b4f2d8322a73627d722fc658
FarrOut <4e4f9ce1@opayq.com>
2023-09-20 11:06:36 +0200

    refactored pipelines from nestedstack to stack

--- TypeScript/lib/stacks/pipelines-stack.ts
+++ TypeScript/lib/stacks/pipelines-stack.ts
@@ -1,28 +1,66 @@
 import * as cdk from 'aws-cdk-lib';
 import {Construct} from 'constructs';
-import {PipelinesNestedStack} from '../components/cicd/pipelines-nestedstack';
 import {LogGroupNestedStack} from "../components/logging/log-group-nestedstack";
 import * as logs from 'aws-cdk-lib/aws-logs';
 
+interface PipelinesProps extends cdk.StackProps {
+    RepositoryOwner: string,
+    RepositoryName: string,
+    BranchName: string,
+    Vpc?: ec2.IVpc,
+    LogGroup?: logs.ILogGroup,
+    removalPolicy?: cdk.RemovalPolicy,
+    SubDir?: string,
+}
+
 export class PipelinesStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+
+    public readonly pipeline: CodePipeline;
+
+    constructor(scope: Construct, id: string, props: PipelinesProps) {
         super(scope, id, props);
-
-        // let Vpc = new VpcNestedStack(this, 'VpcNestedStack',)
-
-        let myRemovalPolicy = cdk.RemovalPolicy.DESTROY;
 
         let MyLogGroup = new LogGroupNestedStack(this, 'LogGroupNestedStack',
             {removalPolicy: myRemovalPolicy, retention: logs.RetentionDays.ONE_WEEK}).logGroup
 
-        let Pipe = new PipelinesNestedStack(this, 'PipelinesNestedStack', {
-            BranchName: "main", RepositoryOwner: "FarrOut",
-            RepositoryName: "SeedyKay",
-            StackName: "PipelinesStack",
-            LogGroup: MyLogGroup,
-            removalPolicy: myRemovalPolicy,
-            stackName: this.stackName,
-        })
+        this.pipeline = new CodePipeline(this, 'Pipeline', {
+            pipelineName: 'MyPipeline',
+            selfMutation: true,
+
+            synth: new ShellStep('Synth', {
+                input: CodePipelineSource.gitHub(props.RepositoryOwner + '/' + props.RepositoryName, props.BranchName),
+                // installCommands: [`cd ${SubDir}`, `pwd`, `ls -la`,
+                //     'npm install -g aws-cdk',
+                // ],
+                commands:
+                    [`cd ${props.SubDir}`, `pwd`,
+                        'npm ci', `npx cdk --version`,
+                        'npm run build',
+                        `npx cdk synth ${this.stackName}`],
+                primaryOutputDirectory: `${props.SubDir}/cdk.out`,
+            }),
+            codeBuildDefaults: {
+                buildEnvironment: {
+                    // privileged: true,
+                    // buildImage: codebuild.LinuxBuildImage.STANDARD_6_0,
+                    // computeType: codebuild.ComputeType.MEDIUM,
+                },
+                partialBuildSpec: codebuild.BuildSpec.fromObject({
+                    env: {
+                        variables: {
+                            // CDK env variables propagation
+                            GIT_BRANCH: props.BranchName,
+                            // NODE_VERSION: '16',
+                        },
+                    },
+                }),
+                logging: {
+                    cloudWatch: {
+                        logGroup: props.LogGroup,
+                    }
+                },
+            },
+        });
 
 
     }
