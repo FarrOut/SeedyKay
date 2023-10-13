254e438a05fa072aff8228e83badb4248e8bfa11
FarrOut <4e4f9ce1@opayq.com>
2023-10-13 11:57:52 +0200

    added cloudwatch dashboard

---
+++ Motley/motley/components/analytics/cloudwatch_dashboard_nestedstack.py
@@ -0,0 +1,48 @@
+from aws_cdk import (
+    # Duration,
+    aws_synthetics_alpha as synthetics, aws_lambda as lambda_, aws_cloudwatch as cloudwatch, aws_ec2 as ec2, aws_sns as sns, aws_cloudwatch_actions as cw_actions,
+    NestedStack, RemovalPolicy, Duration, CfnOutput, )
+from aws_cdk.aws_synthetics_alpha import Code, RuntimeFamily
+from constructs import Construct
+
+
+class CloudWatchDashboardNestedStack(NestedStack):
+
+    def __init__(self, scope: Construct, construct_id: str,
+                 removal_policy: RemovalPolicy = RemovalPolicy.RETAIN,
+                 **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        dashboard = cloudwatch.Dashboard(self, "Dash",)
+        dashboard.apply_removal_policy(removal_policy)
+
+        CfnOutput(self, "DashboardArn", value=dashboard.dashboard_arn,
+                  description='ARN of this dashboard.')
+        CfnOutput(self, "DashboardName", value=dashboard.dashboard_name,
+                  description='The name of this dashboard.')
+
+        dashboard.add_widgets(cloudwatch.GaugeWidget(
+            title="My gauge widget",
+            metrics=[cloudwatch.Metric(
+                metric_name='TunnelState',
+                namespace="AWS/VPN",
+                dimensions_map={
+                    "TunnelIpAddress": "123.123.123.123"
+                },
+                statistic='Minimum'
+            )],
+            left_y_axis=cloudwatch.YAxisProps(
+                min=0,
+                max=1
+            ),
+            annotations=[cloudwatch.HorizontalAnnotation(
+                color="#b2df8d",
+                label="Up",
+                value=1,
+                fill=cloudwatch.Shading.ABOVE,
+            )],
+            statistic="Minimum",
+            period=Duration.minutes(1),
+            width=6,
+            height=6,
+        ))
--- Motley/motley/solutions/cloudwatch_stack.py
+++ Motley/motley/solutions/cloudwatch_stack.py
@@ -3,12 +3,12 @@ from aws_cdk import (
     Stack, RemovalPolicy, aws_ec2 as ec2, aws_sns as sns, Duration,
 )
 from constructs import Construct
+from motley.components.analytics.cloudwatch_dashboard_nestedstack import CloudWatchDashboardNestedStack
 from motley.components.analytics.cloudwatch_lambda_alarms_nestedstack import CloudWatchLambdaAlarmsNestedStack
 from motley.components.networking.vpc_stack import VpcNestedStack
 from motley.computing.lambda_nestedstack import LambdaNestedStack
 from aws_cdk import aws_events as events
 from aws_cdk import aws_events_targets as targets
-
 
 class CloudWatchStack(Stack):
 
@@ -23,25 +23,28 @@ class CloudWatchStack(Stack):
                 self, "VpcStack", removal_policy=removal_policy)
             vpc = net.vpc
 
-        function = LambdaNestedStack(self, "LambdaNestedStack",
-                                     vpc=vpc, removal_policy=removal_policy).function
+        # function = LambdaNestedStack(self, "LambdaNestedStack",
+        #                              vpc=vpc, removal_policy=removal_policy).function
 
-        rule = events.Rule(
-            self, "EatYourMeat",
-            description='How can you have any pudding if you don\'t eat your meat?!',
-            rule_name="EatYourMeat",
-            schedule=events.Schedule.rate(Duration.minutes(1)),
-            targets=[targets.LambdaFunction(function,
-                                            # Optional: set the maxEventAge retry policy
-                                            max_event_age=Duration.hours(2),
-                                            retry_attempts=2
-                                            )],
-        )
-        rule.apply_removal_policy(removal_policy)
+        # rule = events.Rule(
+        #     self, "EatYourMeat",
+        #     description='How can you have any pudding if you don\'t eat your meat?!',
+        #     rule_name="EatYourMeat",
+        #     schedule=events.Schedule.rate(Duration.minutes(1)),
+        #     targets=[targets.LambdaFunction(function,
+        #                                     # Optional: set the maxEventAge retry policy
+        #                                     max_event_age=Duration.hours(2),
+        #                                     retry_attempts=2
+        #                                     )],
+        # )
+        # rule.apply_removal_policy(removal_policy)
 
-        topic = sns.Topic(self, "Topic",
-                          display_name="Lambda alarms"
-                          )
+        # topic = sns.Topic(self, "Topic",
+        #                   display_name="Lambda alarms"
+        #                   )
 
-        CloudWatchLambdaAlarmsNestedStack(
-            self, "CloudWatchLambdaAlarmsNestedStack", function=function, topic=topic, removal_policy=removal_policy)
+        # CloudWatchLambdaAlarmsNestedStack(
+        #     self, "CloudWatchLambdaAlarmsNestedStack", function=function, topic=topic, removal_policy=removal_policy)
+
+        CloudWatchDashboardNestedStack(
+            self, "CloudWatchDashboardNestedStack",  removal_policy=removal_policy)
