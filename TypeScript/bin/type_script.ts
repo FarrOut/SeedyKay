0d3906ab4eff84756b4e8c5ac2b0263f0a296d37
FarrOut <4e4f9ce1@opayq.com>
2023-10-23 12:13:24 +0200

    selective stacks to reduce synth time

--- TypeScript/bin/type_script.ts
+++ TypeScript/bin/type_script.ts
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 // import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import {EksStack} from '../lib/stacks/eks-stack';
-import {LambdaStack} from '../lib/stacks/lambda-stack';
-import {SecurityStack} from '../lib/stacks/security-stack';
-import {PipelinesStack} from '../lib/stacks/pipelines-stack';
+import { EksStack } from '../lib/stacks/eks-stack';
+import { LambdaStack } from '../lib/stacks/lambda-stack';
+import { EventsStack } from '../lib/stacks/events-stack';
+import { SecurityStack } from '../lib/stacks/security-stack';
+import { PipelinesStack } from '../lib/stacks/pipelines-stack';
 import * as blueprints from '@aws-quickstart/eks-blueprints';
-import {ClusterProvider} from "../lib/blueprints/eks/cluster-provider";
-import {networkOverlayAddOns, networkVPCProvider} from "../lib/blueprints/eks/vpc-provider";
-import {NetworkingStack} from "../lib/stacks/network-stack";
+import { ClusterProvider } from "../lib/blueprints/eks/cluster-provider";
+import { networkOverlayAddOns, networkVPCProvider } from "../lib/blueprints/eks/vpc-provider";
+import { NetworkingStack } from "../lib/stacks/network-stack";
+import { AlbStack } from '../lib/stacks/alb-stack';
+import {InstanceStack} from "../lib/stacks/instance-stack";
 
 const app = new cdk.App();
 
@@ -17,39 +20,82 @@ const default_env = {
     region: process.env.CDK_DEFAULT_REGION
 }
 
-const net = new NetworkingStack(app, 'NetworkingStack', {
+
+let enableLambdaStack = false
+let enableEksStack = false
+let enableBluePrintStack = false
+let enableSecurityStack = false
+let enablePipelineStack = false
+let enableLoadBalancingStack = false
+let enableInstanceStack = false
+let enableEventsStack = true
+
+const net = new NetworkingStack(app, 'CdkTypeScriptNetworkingStack', {
     env: default_env,
 });
 
-new LambdaStack(app, 'LambdaStack', {
-    env: default_env,
-    removalPolicy: cdk.RemovalPolicy.DESTROY,
-});
+if (enableInstanceStack) {
+    new InstanceStack(app, 'InstanceStack', {
+        env: default_env,
+        vpc: net.Vpc,
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+}
 
-new EksStack(app, 'EksStack', {
-    env: default_env,
-});
+if (enableLambdaStack) {
+    new LambdaStack(app, 'LambdaStack', {
+        env: default_env,
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+}
+
+if (enableEventsStack) {
+    new EventsStack(app, 'EventsStack', {
+        env: default_env,
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        PrincipalOrgID: app.node.tryGetContext('PrincipalOrgID'),
+    });
+}
 
 
-const EksBluePrintStack = blueprints.EksBlueprint.builder()
-    .account(process.env.CDK_DEFAULT_ACCOUNT)
-    .region(process.env.CDK_DEFAULT_REGION)
-    .clusterProvider(ClusterProvider.SAMPLE)
-    .addOns(...networkOverlayAddOns)
-    .resourceProvider(blueprints.GlobalResources.Vpc, networkVPCProvider)
-    .build(app, 'eks-blueprint');
+if (enableLoadBalancingStack) {
+    new AlbStack(app, 'AlbStack', {
+        env: default_env,
+        vpc: net.Vpc,
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+}
 
-new SecurityStack(app, 'SecurityStack', {
-    env: default_env,
-});
+if (enableEksStack) {
+    new EksStack(app, 'EksStack', {
+        env: default_env,
+    });
+}
 
-new PipelinesStack(app, 'PipelinesStack', {
-    env: default_env,
+if (enableBluePrintStack) {
+    const EksBluePrintStack = blueprints.EksBlueprint.builder()
+        .account(process.env.CDK_DEFAULT_ACCOUNT)
+        .region(process.env.CDK_DEFAULT_REGION)
+        .clusterProvider(ClusterProvider.SAMPLE)
+        .addOns(...networkOverlayAddOns)
+        .resourceProvider(blueprints.GlobalResources.Vpc, networkVPCProvider)
+        .build(app, 'eks-blueprint');
+}
 
-    BranchName: "main",
-    RepositoryOwner: "FarrOut",
-    RepositoryName: "SeedyKay",
-    removalPolicy: cdk.RemovalPolicy.DESTROY,
-    SubDir: "TypeScript"
-});
+if (enableSecurityStack) {
+    new SecurityStack(app, 'SecurityStack', {
+        env: default_env,
+    });
+}
 
+if (enablePipelineStack) {
+    new PipelinesStack(app, 'PipelinesStack', {
+        env: default_env,
+
+        BranchName: "main",
+        RepositoryOwner: "FarrOut",
+        RepositoryName: "SeedyKay",
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        SubDir: "TypeScript"
+    });
+}
