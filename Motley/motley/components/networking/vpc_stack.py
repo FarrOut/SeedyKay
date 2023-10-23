63da707fb76caaa273bdd2db50f9876acc8f710f
FarrOut <4e4f9ce1@opayq.com>
2023-10-23 12:12:31 +0200

    vpc

--- Motley/motley/components/networking/vpc_stack.py
+++ Motley/motley/components/networking/vpc_stack.py
@@ -16,7 +16,55 @@ class VpcNestedStack(NestedStack):
         log = logging.getLogger()
 
         log.info("Creating new VPC")
-        self.vpc = ec2.Vpc(self, 'MyVpc')
+        self.vpc = ec2.Vpc(self, 'MyVpc',
+
+                           # 'IpAddresses' configures the IP range and size of the entire VPC.
+                           # The IP space will be divided based on configuration for the subnets.
+                           ip_addresses=ec2.IpAddresses.cidr("10.0.0.0/16"),
+
+                           # 'maxAzs' configures the maximum number of availability zones to use.
+                           # If you want to specify the exact availability zones you want the VPC
+                           # to use, use `availabilityZones` instead.
+                           max_azs=3,
+
+
+                           # 'subnetConfiguration' specifies the "subnet groups" to create.
+                           # Every subnet group will have a subnet for each AZ, so this
+                           # configuration will create `3 groups × 3 AZs = 9` subnets.
+                           subnet_configuration=[ec2.SubnetConfiguration(
+                               # 'subnetType' controls Internet access, as described above.
+                               subnet_type=ec2.SubnetType.PUBLIC,
+
+                               # 'name' is used to name this particular subnet group. You will have to
+                               # use the name for subnet selection if you have more than one subnet
+                               # group of the same type.
+                               name="Ingress",
+
+                               # 'cidrMask' specifies the IP addresses in the range of of individual
+                               # subnets in the group. Each of the subnets in this group will contain
+                               # `2^(32 address bits - 24 subnet bits) - 2 reserved addresses = 254`
+                               # usable IP addresses.
+                               #
+                               # If 'cidrMask' is left out the available address space is evenly
+                               # divided across the remaining subnet groups.
+                               cidr_mask=24
+                           ), ec2.SubnetConfiguration(
+                               cidr_mask=24,
+                               name="Application",
+                               subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS
+                           ), ec2.SubnetConfiguration(
+                               cidr_mask=28,
+                               name="Database",
+                               subnet_type=ec2.SubnetType.PRIVATE_ISOLATED,
+
+                               # 'reserved' can be used to reserve IP address space. No resources will
+                               # be created for this subnet, but the IP range will be kept available for
+                               # future creation of this subnet, or even for future subdivision.
+                               reserved=True
+                           )
+                           ]
+                           )
+
         self.vpc.apply_removal_policy(removal_policy)
 
         # Only reject traffic and interval every minute.
